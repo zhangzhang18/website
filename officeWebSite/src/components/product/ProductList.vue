34d0a6de2737cf763eda9d2b7eb3d28e7e590547
zhangchunmeng@kuaishou.com <zhangchunmeng@kuaishou.com>
2020-03-04 22:48:17 +0800

    add : json

---
+++ officeWebSite/src/components/product/ProductList.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="product-category">
+    <div class="product-menu">
+      <ul class="menu">
+        <li class="menu-category menu-first">
+          <span>大类</span>
+          <ul class="sub-menu" v-show="isShow">
+            <li>
+              <a>子类</a>
+            </li>
+            <li>
+              <a>子类</a>
+            </li>
+          </ul>
+        </li>
+        <li class="menu-category">
+          <span>大类</span>
+          <ul class="sub-menu" v-show="isShow">
+            <li>
+              <a>子类</a>
+            </li>
+            <li>
+              <a>子类</a>
+            </li>
+            <li>
+              <a>子类</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="product-img">
+      <ul class="detail">
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="img">
+            <a>
+              <img src="@/assets/image/product/list/list-01.jpg" />
+              <p></p>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
